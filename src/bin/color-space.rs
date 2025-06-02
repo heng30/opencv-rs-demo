0c01b664491333b683d910c6734bcc681dbacba0
heng30 <2238288979@qq.com>
2025-06-02 13:25:39 +0800

    [*] update normally

---
+++ src/bin/color-space.rs
@@ -0,0 +1,50 @@
+use anyhow::Result;
+use opencv::{highgui, imgcodecs, imgproc::ColorConversionCodes};
+
+fn main() -> Result<()> {
+    let (w, h) = (640, 480);
+    let window_name = "color";
+    let trackbar_name = "ColorSpace";
+
+    let color_space = [
+        ColorConversionCodes::COLOR_BGR2BGRA,
+        ColorConversionCodes::COLOR_BGR2GRAY,
+        ColorConversionCodes::COLOR_BGR2RGB,
+        ColorConversionCodes::COLOR_BGR2Luv,
+        ColorConversionCodes::COLOR_BGR2YUV,
+        ColorConversionCodes::COLOR_BGR2XYZ,
+        ColorConversionCodes::COLOR_BGR2Lab,
+        ColorConversionCodes::COLOR_BGR2YCrCb,
+        ColorConversionCodes::COLOR_BGR2HSV_FULL,
+        ColorConversionCodes::COLOR_RGB2HLS_FULL,
+    ];
+
+    highgui::named_window(window_name, highgui::WINDOW_NORMAL)?;
+    highgui::resize_window(window_name, w, h)?;
+
+    highgui::create_trackbar(
+        trackbar_name,
+        window_name,
+        None,
+        (color_space.len() - 1) as i32,
+        Some(Box::new(move |_v| {})),
+    )?;
+
+    let mut dst_img = opencv::core::Mat::default();
+    let src_img = imgcodecs::imread("color.png", imgcodecs::IMREAD_COLOR)?;
+
+    loop {
+        let pos = highgui::get_trackbar_pos(trackbar_name, window_name)? as usize;
+        opencv::imgproc::cvt_color(&src_img, &mut dst_img, color_space[pos].into(), 0)?;
+
+        highgui::imshow(window_name, &dst_img)?;
+
+        let key = highgui::wait_key(40)?;
+        if key & 0xFF == 'q' as i32 {
+            break;
+        }
+    }
+
+    highgui::destroy_all_windows()?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,43 @@
 use anyhow::Result;
-use opencv::{
-    core::{Mat, Scalar},
-    highgui,
-    prelude::{MatTrait, MatTraitConst},
-};
-use std::sync::{
-    atomic::{AtomicU8, Ordering},
-    Arc,
-};
+use opencv::{highgui, imgcodecs, imgproc::ColorConversionCodes};
 
 fn main() -> Result<()> {
     let (w, h) = (640, 480);
-    let window_name = "mouse";
-    let (r_value, g_value, b_value) = (
-        Arc::new(AtomicU8::new(0_u8)),
-        Arc::new(AtomicU8::new(0_u8)),
-        Arc::new(AtomicU8::new(0_u8)),
-    );
+    let window_name = "color";
+    let trackbar_name = "ColorSpace";
+
+    let color_space = [
+        ColorConversionCodes::COLOR_BGR2BGRA,
+        ColorConversionCodes::COLOR_BGR2GRAY,
+        ColorConversionCodes::COLOR_BGR2RGB,
+        ColorConversionCodes::COLOR_BGR2Luv,
+        ColorConversionCodes::COLOR_BGR2YUV,
+        ColorConversionCodes::COLOR_BGR2XYZ,
+        ColorConversionCodes::COLOR_BGR2Lab,
+        ColorConversionCodes::COLOR_BGR2YCrCb,
+        ColorConversionCodes::COLOR_BGR2HSV_FULL,
+        ColorConversionCodes::COLOR_RGB2HLS_FULL,
+    ];
 
     highgui::named_window(window_name, highgui::WINDOW_NORMAL)?;
     highgui::resize_window(window_name, w, h)?;
 
-    let rv = r_value.clone();
     highgui::create_trackbar(
-        "Red",
+        trackbar_name,
         window_name,
         None,
-        255,
-        Some(Box::new(move |v| {
-            rv.store(v as u8, Ordering::Relaxed);
-        })),
+        (color_space.len() - 1) as i32,
+        Some(Box::new(move |_v| {})),
     )?;
 
-    let gv = g_value.clone();
-    highgui::create_trackbar(
-        "Green",
-        window_name,
-        None,
-        255,
-        Some(Box::new(move |v| {
-            gv.store(v as u8, Ordering::Relaxed);
-        })),
-    )?;
-
-    let bv = b_value.clone();
-    highgui::create_trackbar(
-        "Blue",
-        window_name,
-        None,
-        255,
-        Some(Box::new(move |v| {
-            bv.store(v as u8, Ordering::Relaxed);
-        })),
-    )?;
-
-    let mut img = Mat::new_rows_cols_with_default(h, w, opencv::core::CV_8UC3, Scalar::all(0.0))?;
+    let mut dst_img = opencv::core::Mat::default();
+    let src_img = imgcodecs::imread("color.png", imgcodecs::IMREAD_COLOR)?;
 
     loop {
-        let rv = r_value.load(Ordering::Relaxed);
-        let gv = g_value.load(Ordering::Relaxed);
-        let bv = b_value.load(Ordering::Relaxed);
+        let pos = highgui::get_trackbar_pos(trackbar_name, window_name)? as usize;
+        opencv::imgproc::cvt_color(&src_img, &mut dst_img, color_space[pos].into(), 0)?;
 
-        for i in 0..img.rows() {
-            for j in 0..img.cols() {
-                let b = ((j as f32 / img.cols() as f32) * bv as f32) as u8;
-                let g = ((i as f32 / img.rows() as f32) * gv as f32) as u8;
-                let r = (((i + j) as f32 / (img.rows() + img.cols()) as f32) * rv as f32) as u8;
-
-                *img.at_2d_mut::<opencv::core::Vec3b>(i, j)? =
-                    opencv::core::Vec3b::from_array([b, g, r]);
-            }
-        }
-
-        highgui::imshow(window_name, &img)?;
+        highgui::imshow(window_name, &dst_img)?;
 
         let key = highgui::wait_key(40)?;
         if key & 0xFF == 'q' as i32 {
