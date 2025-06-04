c6673360e04c55a71c6e262b147cb6e668c88bcf
heng30 <2238288979@qq.com>
2025-06-04 16:27:44 +0800

    [*] update normally

---
+++ src/bin/img-bitwise-xor.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use opencv::{core, highgui, imgcodecs};
+
+fn main() -> Result<()> {
+    let (w, h) = (640, 480);
+    let window_name = "img-bitwise-xor";
+    let img1 = imgcodecs::imread("test.png", imgcodecs::IMREAD_COLOR)?;
+    let img2 = imgcodecs::imread("color.png", imgcodecs::IMREAD_COLOR)?;
+
+    let mut result = core::Mat::default();
+    core::bitwise_xor(&img1, &img2, &mut result, &core::no_array())?;
+
+    highgui::named_window(window_name, highgui::WINDOW_NORMAL)?;
+    highgui::resize_window(window_name, w, h)?;
+
+    loop {
+        highgui::imshow(window_name, &result)?;
+
+        let key = highgui::wait_key(0)?;
+        if key & 0xFF == 'q' as i32 {
+            break;
+        }
+    }
+
+    highgui::destroy_all_windows()?;
+    Ok(())
+}
---
+++ src/bin/img-resize.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use opencv::{core, highgui, imgcodecs};
+
+fn main() -> Result<()> {
+    let (w, h) = (640, 480);
+    let window_name = "img-resize";
+    let img = imgcodecs::imread("test.png", imgcodecs::IMREAD_COLOR)?;
+
+    let mut result = core::Mat::default();
+    opencv::imgproc::resize(
+        &img,
+        &mut result,
+        core::Size::default(), // ignore
+        0.4,
+        0.4,
+        opencv::imgproc::INTER_AREA,
+    )?;
+
+    highgui::named_window(window_name, highgui::WINDOW_NORMAL)?;
+    highgui::resize_window(window_name, w, h)?;
+
+    loop {
+        highgui::imshow(window_name, &result)?;
+
+        let key = highgui::wait_key(0)?;
+        if key & 0xFF == 'q' as i32 {
+            break;
+        }
+    }
+
+    highgui::destroy_all_windows()?;
+    Ok(())
+}
